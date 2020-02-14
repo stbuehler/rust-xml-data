fcce3101476ad2b7d5bc1b08a8bbd802d7272bc2
Eli Flanagan <efx@users.noreply.github.com>
2020-02-14 11:58:39 -0500

    add a simple usage example
    I enjoy skimming examples to quickly understand how to use a library. I
    decided to add one here to help future users.

---
+++ xml-data/examples/struct_custom_tag.rs
@@ -0,0 +1,29 @@
+// use `cargo run -p xml-data --example struct_custom_tag` to run from git repository base directory
+
+use std::borrow::Cow;
+use xml_data::{
+	quick_xml::serialize_document,
+	Element,
+};
+
+/// a struct that customizes the tag name in XML serialization
+#[derive(Element)]
+#[xml_data("datum")]
+pub struct Data {
+	#[xml_data(attr_string)]
+	pub key: Cow<'static, str>,
+	#[xml_data(attr)]
+	pub other: u32,
+}
+
+fn main() {
+	let stuff = Data {
+		key: Cow::from("hello"),
+		other: 5,
+	};
+
+	eprintln!("Generating XML output...");
+
+	// print serialized document
+	println!("{}", serialize_document(&stuff).unwrap());
+}
